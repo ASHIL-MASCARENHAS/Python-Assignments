29a55a0193a01a5d4fb70bd381e8e93f736bdd71
ASHIL-MASCARENHAS <ashilmascarenhas@gmail.com>
2025-05-21 20:25:31 +0530

    Added Assignment 3 math function

---
+++ ASSIGNMENT3/assignment3_02.py
@@ -0,0 +1,16 @@
+import math
+
+num=float(input("Enter a number: "))
+
+def squareRoot(num):
+    return math.sqrt(num)
+
+def log(num):
+    return math.log(num)
+
+def sine(num):
+    return math.sin(num)
+
+print("Square root:",squareRoot(num))
+print("Logarithm:",log(num))
+print("Sine:",sine(num))
